6709f0294d76262e06e707704ef71cc284e31e88
gloriaJun <pureainu@gmail.com>
2019-04-19 02:08:16 +0900

    Delete no need file

--- build/config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-const PROJECT_ROOT = path.resolve(__dirname, '../');
-
-module.exports = {
-  projectRoot: PROJECT_ROOT,
-  appEntry: path.join(PROJECT_ROOT, 'src'),
-  outputPath: path.join(PROJECT_ROOT, 'dist'),
-};
