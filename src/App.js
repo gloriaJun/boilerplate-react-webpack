1c8dfdec52ee3703396271ec987f35989c8e6f54
gloriaJun <pureainu@gmail.com>
2019-05-31 10:51:06 +0900

    Fix lint error

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
 
 const App = () => {
   return (
     <>
       <h1>Hello React App!!!</h1>
-    <BrowserRouter>
-    </BrowserRouter>
     </>
   );
 };
